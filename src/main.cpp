5082bfaae7edfcf63b72e31140c9319a8d3a1b7b
entity12208 <116198380+entity12208@users.noreply.github.com>
2025-01-01 09:34:49 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayerObject.hpp>
 #include <Geode/modify/GameManager.hpp>
-#include <Geode/utils/file.hpp>
-#include <Geode/utils/cocos.hpp>
-#include <cocos2d.h> // Include the Cocos2d header for EventKeyboard
 #include <fstream>
 #include <vector>
 #include <unordered_map>
@@ -18,15 +15,7 @@ public:
     static inline bool playingBack = false;
     static inline size_t playbackIndex = 0;
 
-    void onEnable() {
-        resetState();
-    }
-
-    void onDisable() {
-        resetState();
-    }
-
-    void resetState() {
+    static void resetState() {
         jumpAttempts.clear();
         successfulJumps.clear();
         jumpPlayback.clear();
@@ -46,18 +35,18 @@ class $modify(PlayerObject) {
                 JumpBot::successfulJumps.push_back(deathPos);
             }
         }
-        this->die(unknown);
+        PlayerObject::die(unknown);
     }
 
     void update(float dt) {
         if (JumpBot::playingBack && JumpBot::playbackIndex < JumpBot::jumpPlayback.size()) {
             float currentPos = this->getPositionX();
             if (currentPos >= JumpBot::jumpPlayback[JumpBot::playbackIndex]) {
-                this->jump(false); 
+                this->jump(false);
                 JumpBot::playbackIndex++;
             }
         }
-        this->update(dt);
+        PlayerObject::update(dt);
     }
 };
 
@@ -71,35 +60,50 @@ class $modify(GameManager) {
             outFile.close();
             JumpBot::successfulJumps.clear();
         }
-        this->levelComplete();
+        GameManager::levelComplete();
     }
 };
 
-class JumpBotMod : public cocos2d::EventListenerKeyboard { // Inherit from EventListenerKeyboard
+class JumpBotMod : public Geode::EventListener {
 public:
-    bool onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event) override { // Use EventKeyboard::KeyCode and Event
-        if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_TAB && cocos2d::EventKeyboard::KeyCode::KEY_ALT) {
-            if (!JumpBot::recording && !JumpBot::playingBack) {
-                JumpBot::recording = true;
-                JumpBot::jumpAttempts.clear();
-                JumpBot::successfulJumps.clear();
-            } else if (JumpBot::recording) {
-                JumpBot::recording = false;
-                JumpBot::playingBack = true;
-                JumpBot::playbackIndex = 0;
-
-                std::ifstream inFile(JumpBot::levelName + ".txt");
-                float pos;
-                while (inFile >> pos) {
-                    JumpBot::jumpPlayback.push_back(pos);
-                }
-                inFile.close();
-            } else if (JumpBot::playingBack) {
-                JumpBot::playingBack = false;
-                JumpBot::jumpPlayback.clear();
-                JumpBot::playbackIndex = 0;
+    void setup() {
+        auto dispatcher = Geode::getEventDispatcher();
+        dispatcher->registerEvent<Geode::KeyEvent>([this](Geode::KeyEvent* event) {
+            if (event->getKeyCode() == Geode::KeyCode::KEY_TAB && event->isAltPressed()) {
+                handleKeyEvent();
+                return true;
             }
+            return false;
+        });
+    }
+
+    void handleKeyEvent() {
+        if (!JumpBot::recording && !JumpBot::playingBack) {
+            JumpBot::recording = true;
+            JumpBot::jumpAttempts.clear();
+            JumpBot::successfulJumps.clear();
+        } else if (JumpBot::recording) {
+            JumpBot::recording = false;
+            JumpBot::playingBack = true;
+            JumpBot::playbackIndex = 0;
+
+            std::ifstream inFile(JumpBot::levelName + ".txt");
+            float pos;
+            while (inFile >> pos) {
+                JumpBot::jumpPlayback.push_back(pos);
+            }
+            inFile.close();
+        } else if (JumpBot::playingBack) {
+            JumpBot::playingBack = false;
+            JumpBot::jumpPlayback.clear();
+            JumpBot::playbackIndex = 0;
         }
-        return true;
     }
 };
+
+// Initialize the Geode mod
+extern "C" GEODE_API void GEODE_CALL geode_load(Mod* mod) {
+    JumpBotMod* listener = new JumpBotMod();
+    listener->setup();
+    JumpBot::resetState();
+}
